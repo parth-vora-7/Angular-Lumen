f5fea8e8d5a2d26949939b09f407d722c40f95fe
xparthx <parth.vora.777@gmail.com>
2016-06-09 11:43:37 +0530

    Display data on table

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 var app = angular.module('ngApp', ['datatables', 'ngResource']);
 app.constant('END_POINT', 'lumen/public');
-app.controller('ngCtrl', ['$scope', '$resource', 'fetchUsers', 'DTOptionsBuilder', 'DTColumnBuilder', function ($scope, $resource, fetchUsers, DTOptionsBuilder, DTColumnBuilder) {
-    $scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withDisplayLength(2);
-    fetchUsers.query().$promise.then(function(users) {
-      $scope.users = users;
-      console.log(users);
-    });
+app.controller('ngCtrl', ['$location', 'END_POINT', '$scope', 'DTOptionsBuilder', 'DTColumnBuilder',
+    function ($location, END_POINT, $scope, DTOptionsBuilder, DTColumnBuilder) {
+    $scope.dtOptions = DTOptionsBuilder.fromSource($location.$$absUrl + END_POINT + '/users'); //newOptions().withPaginationType('full_numbers').withDisplayLength(2);
     $scope.dtColumns = [
-      DTColumnBuilder.newColumn('id').withOption('ID', 'asd'),
+        DTColumnBuilder.newColumn('first_name').withTitle('First name'),
+        DTColumnBuilder.newColumn('last_name').withTitle('Last name'),
+        DTColumnBuilder.newColumn('email').withTitle('Email'),
+        DTColumnBuilder.newColumn('salary').withTitle('Salary'),
+        DTColumnBuilder.newColumn('occupation').withTitle('Occupation'),
+        DTColumnBuilder.newColumn('phone_no').withTitle('Phone no'),
+        DTColumnBuilder.newColumn('address').withTitle('Address')
     ];
-//    $scope.dtOptions = DTOptionsBuilder.fromFnPromise(function () {
-//      console.log(fetchUsers.query());
-//    }).withPaginationType('full_numbers');
   }]);
 
-app.factory('fetchUsers', function ($http, $location, $resource, END_POINT) {
-  return $resource($location.$$absUrl + END_POINT + '/users');
+app.factory('fetchUsers', function ($location, $resource, END_POINT) {
+    return 1;
 });
