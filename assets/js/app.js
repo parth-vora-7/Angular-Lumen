5735e59efadd348728aee280e3495c5ea126d7fa
parthvora <parth@ossquad.com>
2016-06-09 21:00:30 +0530

    Col reorder

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,28 @@
-var app = angular.module('ngApp', ['datatables', 'ngResource']);
+var app = angular.module('ngApp', ['datatables', 'ngResource', 'datatables.colreorder', 'datatables.columnfilter']);
 app.constant('END_POINT', 'lumen/public');
 app.controller('ngCtrl', ['$location', 'END_POINT', '$scope', 'DTOptionsBuilder', 'DTColumnBuilder',
-    function ($location, END_POINT, $scope, DTOptionsBuilder, DTColumnBuilder) {
+  function ($location, END_POINT, $scope, DTOptionsBuilder, DTColumnBuilder) {
     $scope.dtOptions = DTOptionsBuilder.newOptions().
-            withOption('ajax', { 
+            withOption('ajax', {
               url: $location.$$absUrl + END_POINT + '/users', type: 'post'
             })
             .withDataProp('data')
             .withOption('serverSide', true)
-            .withOption('processing', true);
-    //newOptions().withPaginationType('full_numbers').withDisplayLength(2);
+            .withOption('processing', true)
+            .withColReorder();
+    
     $scope.dtColumns = [
-        DTColumnBuilder.newColumn('first_name').withTitle('First name'),
-        DTColumnBuilder.newColumn('last_name').withTitle('Last name'),
-        DTColumnBuilder.newColumn('email').withTitle('Email'),
-        DTColumnBuilder.newColumn('salary').withTitle('Salary'),
-        DTColumnBuilder.newColumn('occupation').withTitle('Occupation'),
-        DTColumnBuilder.newColumn('phone_no').withTitle('Phone no'),
-        DTColumnBuilder.newColumn('address').withTitle('Address')
+      DTColumnBuilder.newColumn('id').notVisible(),
+      DTColumnBuilder.newColumn('first_name').withTitle('First name'),
+      DTColumnBuilder.newColumn('last_name').withTitle('Last name'),
+      DTColumnBuilder.newColumn('email').withTitle('Email'),
+      DTColumnBuilder.newColumn('salary').withTitle('Salary'),
+      DTColumnBuilder.newColumn('occupation').withTitle('Occupation'),
+      DTColumnBuilder.newColumn('phone_no').withTitle('Phone no'),
+      DTColumnBuilder.newColumn('address').withTitle('Address')
     ];
   }]);
 
 app.factory('fetchUsers', function ($location, $resource, END_POINT) {
-    return 1;
+  return 1;
 });
